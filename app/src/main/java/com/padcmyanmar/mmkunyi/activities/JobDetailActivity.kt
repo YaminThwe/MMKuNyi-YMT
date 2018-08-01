628325d07d47f90eb9a23fcbf575f55badaebbce
Yamin Thwe <yaminthwe1730@gmail.com>
2018-08-01 23:57:56 +0630

    MMKuNyi_YMT_2

--- app/src/main/java/com/padcmyanmar/mmkunyi/activities/JobDetailActivity.kt
+++ app/src/main/java/com/padcmyanmar/mmkunyi/activities/JobDetailActivity.kt
@@ -1,25 +1,29 @@
 package com.padcmyanmar.mmkunyi.activities
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.view.menu.MenuView
 import android.util.Log
+import android.view.View
 import com.padcmyanmar.mmkunyi.R
 import com.padcmyanmar.mmkunyi.data.models.JobsModel
 import com.padcmyanmar.mmkunyi.data.vos.JobsVO
 import com.padcmyanmar.mmkunyi.delegate.JobsDelegate
 import com.padcmyanmar.mmkunyi.utils.JobsConstants
 import kotlinx.android.synthetic.main.activity_job_details.*
+import kotlinx.android.synthetic.main.view_holder_jobs_list.*
 
-class JobDetailActivity: AppCompatActivity() {
-
+class JobDetailActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_details)
 
-        var jobsId:Int = intent.getIntExtra(JobsConstants.JOB_ID,1)
+        var jobsId: Int = intent.getIntExtra(JobsConstants.JOB_ID, 1)
 
         val jobsVO: JobsVO = JobsModel.getInstance().getJobsById(jobsId)!!
         BindData(jobsVO)
@@ -27,9 +31,33 @@ class JobDetailActivity: AppCompatActivity() {
 
     }
 
+    private var importantNote: String = ""
+
+    @SuppressLint("StringFormatInvalid", "SetTextI18n")
     private fun BindData(jobsVO: JobsVO?) {
-        tvJobDescription!!.text = jobsVO?.shortDesc
+        tvJobDescription!!.text = jobsVO?.fullDesc
+        tvPlaceInDetail!!.text = jobsVO?.location
+        tvAmountInDetail!!.text = jobsVO?.offerAmount!!.amount.toString()
+        tvDurationInDetail!!.text = jobsVO?.offerAmount!!.duration
+
+        if (jobsVO?.importantNotes.size == 1) {
+            tvImportantNotes!!.text = jobsVO.importantNotes[0]
+        } else {
+            importantNote += jobsVO?.importantNotes[0]
+            for (importantNotesId in 1 until jobsVO?.importantNotes.size) {
+                if (importantNotesId != jobsVO?.importantNotes.size) {
+                    var notes: String = jobsVO?.importantNotes[importantNotesId]
+                    importantNote += "\n" + notes
+                } else break
+            }
+            tvImportantNotes!!.text = importantNote
+        }
+
+
+
+
     }
 
 
 }
+
--- app/src/main/java/com/padcmyanmar/mmkunyi/viewholders/JobsListViewHolder.kt
+++ app/src/main/java/com/padcmyanmar/mmkunyi/viewholders/JobsListViewHolder.kt
@@ -2,21 +2,29 @@ package com.padcmyanmar.mmkunyi.viewholders
 
 import android.view.View
 import com.bumptech.glide.Glide
+import com.padcmyanmar.mmkunyi.R
+import com.padcmyanmar.mmkunyi.R.id.tvPostedDate
 import com.padcmyanmar.mmkunyi.data.vos.JobsVO
 import com.padcmyanmar.mmkunyi.delegate.JobsDelegate
 import kotlinx.android.synthetic.main.activity_job_details.view.*
+import kotlinx.android.synthetic.main.view_holder_jobs_list.*
 import kotlinx.android.synthetic.main.view_holder_jobs_list.view.*
 
-class JobsListViewHolder(itemView: View, private val jobsDelegate: JobsDelegate) : BaseViewHolder<JobsVO> (itemView) {
+class JobsListViewHolder(itemView: View, private val jobsDelegate: JobsDelegate) : BaseViewHolder<JobsVO>(itemView) {
     override fun setData(data: JobsVO) {
 
         kuNyiData = data
 
-        itemView.tvShortDescription !!.text = data.shortDesc
+        itemView.tvShortDescription!!.text = data.shortDesc
+        itemView.tvPlace!!.text = data.location
+        itemView.tvAmount!!.text = data.offerAmount!!.amount.toString()
+        itemView.tvDuration!!.text = data.offerAmount!!.duration
+
+        val tvPostDate=itemView.resources.getString(R.string.posted_date, data.postedDate!!)
+        itemView.tvPostedDate!!.text = tvPostDate
 
 
     }
-
 
 
     override fun onClick(p0: View?) {
